79a248e12a078d6a925dda17cc9632b5261e6b25
yokwe <hasegawa.yasuhiro+github@gmail.com>
2016-08-31 16:48:28 +0900

    Add code to enumerate sheet in ods file.

--- calc/src/study/calc/T001.java
+++ calc/src/study/calc/T001.java
@@ -1,10 +1,19 @@
 package study.calc;
 
+import com.sun.star.beans.PropertyState;
 import com.sun.star.beans.PropertyValue;
 import com.sun.star.comp.helper.Bootstrap;
+import com.sun.star.container.XEnumeration;
+import com.sun.star.container.XEnumerationAccess;
+import com.sun.star.container.XIndexAccess;
+import com.sun.star.container.XNamed;
+import com.sun.star.document.UpdateDocMode;
 import com.sun.star.frame.XComponentLoader;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiComponentFactory;
+import com.sun.star.sheet.XSpreadsheet;
+import com.sun.star.sheet.XSpreadsheetDocument;
+import com.sun.star.sheet.XSpreadsheets;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
 
@@ -22,9 +31,47 @@ public class T001 {
 			XMultiComponentFactory xLocalServiceManager = xLocalContext.getServiceManager();
 			Object desktop = xLocalServiceManager.createInstanceWithContext("com.sun.star.frame.Desktop", xLocalContext);
 			XComponentLoader xComponentLoader = (XComponentLoader) UnoRuntime.queryInterface(XComponentLoader.class, desktop);
-			PropertyValue[] pPropValues = new PropertyValue[0];
-			// Use "private:factory/scalc" for calc
-			XComponent xComponent = xComponentLoader.loadComponentFromURL("private:factory/swriter", "_blank", 0, pPropValues);
+			
+            PropertyValue [] props = new com.sun.star.beans.PropertyValue[] {
+                new PropertyValue("ReadOnly", 0, true, PropertyState.DIRECT_VALUE),
+                new PropertyValue("UpdateDocMode", 0, UpdateDocMode.QUIET_UPDATE, PropertyState.DIRECT_VALUE),
+            };
+
+			String url = "file:///home/hasegawa/Dropbox/Trade/投資損益計算_2016_SAVE.ods";  // existing file
+			XComponent xComponent = xComponentLoader.loadComponentFromURL(url, "_blank", 0, props);
+			
+			XSpreadsheets sheets = UnoRuntime.queryInterface(XSpreadsheetDocument.class, xComponent).getSheets();
+			
+			// Enumerate all sheet using index access
+			{
+				XIndexAccess indexAccess = UnoRuntime.queryInterface(XIndexAccess.class, sheets);
+				System.out.println("index = " + indexAccess.getCount());
+				
+				for(int i = 0; i < indexAccess.getCount(); i++) {
+					XSpreadsheet sheet = UnoRuntime.queryInterface(XSpreadsheet.class, indexAccess.getByIndex(i));
+					XNamed named = UnoRuntime.queryInterface(XNamed.class, sheet);
+					System.out.println(i + " : " + named.getName());
+				}
+			}
+			
+			// Enumerate all sheet using enumeration
+			{
+				XEnumerationAccess enumerateAccess = UnoRuntime.queryInterface(XEnumerationAccess.class, sheets);
+				XEnumeration eumeration = enumerateAccess.createEnumeration();
+				while(eumeration.hasMoreElements()) {
+					XSpreadsheet sheet = UnoRuntime.queryInterface(XSpreadsheet.class, eumeration.nextElement());
+					XNamed named = UnoRuntime.queryInterface(XNamed.class, sheet);
+					System.out.println("** " + named.getName());
+				}
+			}
+			
+			
+			
+			System.out.println("Before SLEEP");
+			Thread.sleep(1000);
+			System.out.println("AFter SLEEP");
+			
+			xComponent.dispose();
 		} catch (Throwable e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
